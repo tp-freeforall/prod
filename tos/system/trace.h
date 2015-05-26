7b39fa2a20c7124fc59f5206104b1666a77b8369
Eric B. Decker <cire831@gmail.com>
2015-05-25 19:32:55 -0700

    trace: add check for TRACE_SIZE
    add check for TRACE_SIZE when platform_trace hasn't constructed
    properly and Trace is being used.

--- tos/system/trace.h
+++ tos/system/trace.h
@@ -1,5 +1,5 @@
 /**
- * Copyright 2008, 2013 (c) Eric B. Decker
+ * Copyright 2008, 2013, 2015 (c) Eric B. Decker
  * All rights reserved.
  *
  * @author Eric B. Decker, <cire831@gmail.com>
@@ -9,6 +9,11 @@
 #define __TRACE_H__
 
 #include "platform_trace.h"
+
+#ifndef TRACE_SIZE
+#warning TRACE_SIZE not defined by platform_trace, defaulting to 1
+#define TRACE_SIZE 1
+#endif
 
 typedef struct {
   uint32_t stamp;
