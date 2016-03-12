87c0b7481d810b576f945bbf373972708652e491
Eric B. Decker <cire831@gmail.com>
2016-03-12 02:18:57 -0800

    x5/usci: update documentation

--- tos/chips/msp430/x5xxx/usci-v2/msp430usci.h
+++ tos/chips/msp430/x5xxx/usci-v2/msp430usci.h
@@ -1,7 +1,65 @@
+/*
+ * Copyright (c) 2010-2011, 2016 Eric B. Decker
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * - Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ *
+ * - Redistributions in binary form must reproduce the above copyright
+ *   notice, this list of conditions and the following disclaimer in the
+ *   documentation and/or other materials provided with the
+ *   distribution.
+ *
+ * - Neither the name of the copyright holders nor the names of
+ *   its contributors may be used to endorse or promote products derived
+ *   from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL
+ * THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @author Eric B. Decker <cire831@gmail.com>
+ *
+ * Support the x5 version of the USCI for the TI MSPx{5,6}xxx processors,
+ * (see TI MSP430x{5,6}xxx Users guide slau208).
+ *
+ * This is somewhat based on the x2 USCI interface, however, the x2 is seriously
+ * screwed (see tos/chips/msp430/x2xxx/usci/msp430usci.h).  This is somewhat
+ * cleaned up in the x5.  The processor include files from TI tell the story...
+ *
+ * x1:  __MSP430_HAS_UART0__	usart0 present
+ *	__MSP430_HAS_I2C__	usart0 has I2C
+ *	__MSP430_HAS_UART1__
+ *
+ * x2:	__MSP430_HAS_USCI__
+ *	__MSP430_HAS_USCI_AB0__	indicates interrupts messy.
+ *	__MSP430_HAS_USCI_AB1__
+ *
+ * x5:	__MSP430_HAS_USCI_A0__	indicates vectors are module specific.
+ *	__MSP430_HAS_USCI_B0__
+ *	__MSP430_HAS_USCI_A1__
+ *	__MSP430_HAS_USCI_B1__	etc.
+ */
+
 #ifndef _H_Msp430Usci_h
 #define _H_Msp430Usci_h
 
 #include "msp430hardware.h"
+
+//#define X5_USCI
 
 #ifndef UCSSEL__SMCLK
 
